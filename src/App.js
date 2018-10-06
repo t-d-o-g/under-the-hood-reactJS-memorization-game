0b4fe5b690f5bf2d87dca341370f03302c8c9765
Travis Collins <trc229@gmail.com>
2018-10-06 05:51:15 -0400

    create-react-app boilerplate setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="page-header jumbotron fluid-jumbotron text-center">
+          <div className="container">
+            <h1>Under-the-hood-ReactJS</h1>
+            <h2>Memorization Game</h2>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
