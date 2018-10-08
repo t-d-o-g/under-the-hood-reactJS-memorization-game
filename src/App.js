9dae6187d81141e389d2dce5c287db9080e6579d
Travis Collins <trc229@gmail.com>
2018-10-07 20:39:26 -0400

    Randomize and display mounting scheme

--- src/App.js
+++ src/App.js
@@ -1,37 +1,49 @@
 import React, { Component } from 'react';
 import SchemeCard from './components/SchemeCard';
 import Wrapper from './components/Wrapper';
-import schemes from './schemes.json';
+import mountingScheme from './mounting-scheme.json';
+
+function shuffleArray(arr) {
+  let currentIndex = arr.length, tempVal, randIndex;
+
+  while(0 !== currentIndex) {
+    randIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    tempVal = arr[currentIndex];
+    arr[currentIndex] = arr[randIndex];
+    arr[randIndex] = tempVal;
+  }
+
+  return arr;
+};
 
 class App extends Component {
   state = {
-    schemes
-  };
-
-  selectedScheme = id => {
-    const schemes = this.state.schemes.filter(scheme => scheme.id !== id);
-    this.setState({ schemes });
+    mountingScheme
   };
 
   render() {
+    const shuffledSchemes = shuffleArray(mountingScheme);
     return (
-      // <div className="App">
-      //   <div className="page-header jumbotron fluid-jumbotron text-center">
-      //     <div className="container">
-      //       <h1>Under-the-hood-ReactJS</h1>
-      //       <h2>Memorization Game</h2>
-      //     </div>
-      //   </div>
+      <div className="App">
+        <div className="page-header jumbotron fluid-jumbotron">
+          <div className="container">
+            <h1>Under-the-hood-ReactJS</h1>
+            <h2>Memorization Game</h2>
+          </div>
+        </div>
         <Wrapper>
-          {this.state.schemes.map(scheme => (
-            <SchemeCard
-              selectedScheme={this.selectedScheme}
+          {shuffledSchemes.map(scheme => {
+            return <SchemeCard
+              key={scheme.id}
               id={scheme.id}
+              name={scheme.name}
               image={scheme.image}
             />
-          ))}
+          })}
         </Wrapper>
-      // </div>
+      </div>
     );
   }
 }
--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import './Wrapper.css';
 
-const Wrapper = props => <div className="wrapper">{props.children}</div>
+// const Wrapper = props => <div className="wrapper">{props.children}</div>
+
+const Wrapper = props => (
+  <div className="wrapper container">
+    <div className="row py-3">
+      <div className="col">{props.children[0]}</div>
+    </div>
+    <div className="row py-3">
+      <div className="col">{props.children[1]}</div>
+      <div className="col">{props.children[2]}</div>
+      <div className="col">{props.children[3]}</div>
+    </div>
+    <div className="row py-3">
+      <div className="col">{props.children[4]}</div>
+      <div className="col">{props.children[5]}</div>
+    </div>
+    <div className="row py-3">
+      <div className="col">{props.children[6]}</div>
+      <div className="col">{props.children[7]}</div>
+    </div>
+  </div>
+)
 
 export default Wrapper;
