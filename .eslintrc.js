ace1f7ae223c03dff8a5b830d8934980685b18ef
Travis Collins <trc229@gmail.com>
2018-10-09 05:40:13 -0400

    Add eslint with airbnb dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "extends": "airbnb",
+  "env": {
+    "es6": true,
+    "browser": true
+  },
+  "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    }
+}
